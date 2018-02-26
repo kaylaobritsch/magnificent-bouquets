b63a4fbaf30f7d6097c56aabc54ccf61ee6a48b7
kaylaobritsch <obritschk@gmail.com>
2018-02-25 23:03:14 -0500

    Upload funeral flowers
    Fix bugs such as spacing, hero banner timeout, modal bugs, and small screen issues. The modal has been made smaller and navigation has moved because all images are different sizes.

--- js/hero-banner.js
+++ js/hero-banner.js
@@ -1,11 +1,14 @@
 var slideIndex = 1;
+var timer = null;
 showDivs(slideIndex);
 
 function plusDivs(n) {
+  clearTimeout(timer);
   showDivs(slideIndex += n);
 }
 
 function currentDiv(n) {
+  clearTimeout(timer);
   showDivs(slideIndex = n);
 }
 
@@ -13,20 +16,30 @@ function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("badge");
-  if (n==undefined){n = ++slideIndex}
+
+  if (n==undefined) {
+    n = ++slideIndex
+  }
+
   if (n > x.length) {
     slideIndex = 1
   }
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-     x[slideIndex-1].className += " animate-fading";
+
+  if (n < 1) {
+    slideIndex = x.length
   }
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    x[slideIndex-1].className += " animate-fading";
+  }
+
   for (i = 0; i < dots.length; i++) {
      dots[i].className = dots[i].className.replace(" badge-focus", "");
   }
+
   x[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " badge-focus";
 
-  setTimeout(showDivs, 10000);
+  timer = setTimeout(showDivs, 6000);
 }
